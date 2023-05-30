2f81a870173e60da0b728cd72932e5153dd4dee5
Usmonxoja04 <usmonxojamoydinov3@gmail.com>
2023-05-30 13:53:00 +0200

    1st days homework is done

--- M1/M1-D1-HW.js
+++ M1/M1-D1-HW.js
@@ -1,1 +1,27 @@
-let smtth
+// exercise 3
+let number = 12 + 20;
+console.log("The result is " + number)
+
+//exercise 4
+let x = 12
+console.log("x = " + x)
+
+//exercise 5
+let name = "John Doe"
+console.log("The name of the person is " + name)
+
+//exercise 6
+let y = 12
+let result = x - y
+console.log("result shunga teng " ,result)
+
+//exercise 7
+let name1 = "john"
+let name2 = "John"
+let result1 = name1 == name2
+console.log ("Bu ikkala varaible tengmi ",result1) 
+
+//exercise 8
+let numericValueLessThan10 = 2
+let two = numericValueLessThan10 == 2;
+console.log(two ? "two" : "false");
