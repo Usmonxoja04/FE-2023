1220df6a65918b06b0fd94317e4a7226bbe06670
Usmonxoja04 <usmonxojamoydinov3@gmail.com>
2023-06-05 20:26:50 +0200

    d6 is finished

---
+++ D5/M1-D5-HW-extra.js
@@ -0,0 +1,8 @@
+/* EXTRA 1
+ Write a function called "checkArray" which receives an array of random numbers (created with giveMeRandom)
+  and prints, for each item, whether it's bigger than 5 or not.
+ The function should return the sum of just the numbers bigger than 5.
+*/
+function loopUntil (x){
+   let count 
+}
